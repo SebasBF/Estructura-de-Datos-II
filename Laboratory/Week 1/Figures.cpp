97a7ae9590db593fbcf281cd6c396cb79a7c4b19
Sebastián Batista Ferrera <sebastianbatistaferrera@gmail.com>
2025-02-06 14:51:40 -0400

    figures done

--- Laboratory/Week 1/Figures.cpp
+++ Laboratory/Week 1/Figures.cpp
@@ -1,25 +1,67 @@
 #include <iostream>
 using namespace std;
 
-void rightTriangle(int n){
+void rightTriangle(int n, int spaces){
+    if(spaces == 0){ return; }
+
+    for(int i = 0; i < spaces; i++){ 
+        cout << " "; 
+    }
+
+    for(int i = 0; i <= (n - spaces) ; i++){
+        cout << "*";
+    }
     
+    cout << endl;
 
+    return rightTriangle(n, spaces - 1);
 }
 
-void equilateralTriangle(){
+void equilateralTriangle(int n, int spaces){
+    if(spaces == 0){ return; }
 
+    for(int i = 0; i < spaces; i++){ 
+        cout << " "; 
+    }
 
+    for(int i = 0; i <= 2*(n - spaces) ; i++){
+        cout << "*";
+    }
+ 
+    cout << endl;
+
+    return equilateralTriangle(n, spaces - 1);
 }
 
-void invertedTriangle(){
+void invertedTriangle(int n, int spaces = 1){
+    
+    if(spaces > n){ return; }
 
+    for(int i = 0; i < spaces; i++){ 
+        cout << " "; 
+    }
 
+    for(int i = 0; i <=  2*(n - spaces) ; i++){
+        cout << "*";
+    }
+ 
+    cout << endl;
 
+    return invertedTriangle(n, spaces + 1);
 }
 
 
 int main(){
     int levels;
-    cout << "Indicate how much levels the figures will have: ";
+
+    cout << "Indicate how many levels the figures will have: ";
     cin >> levels;
+    cout << endl;
+
+    rightTriangle(levels, levels);
+    cout << endl;
+    equilateralTriangle(levels,levels);
+    cout << endl;
+    invertedTriangle(levels);
+    
 }
