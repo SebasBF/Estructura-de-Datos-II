d2eb3b87ff037a7dae0605463ebf66666e1d54ee
Sebastián Batista Ferrera <sebastianbatistaferrera@gmail.com>
2025-02-06 12:06:47 -0400

    first commit

---
+++ Laboratory/Week 1/Figures.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void rightTriangle(int n){
+    
+
+}
+
+void equilateralTriangle(){
+
+
+}
+
+void invertedTriangle(){
+
+
+
+}
+
+
+int main(){
+    int levels;
+    cout << "Indicate how much levels the figures will have: ";
+    cin >> levels;
+}
