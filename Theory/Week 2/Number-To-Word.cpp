8509c9a1fe34bd463047e4eaf3e3d3ad44ac6eb4
Sebastián Batista Ferrera <sebastianbatistaferrera@gmail.com>
2025-02-17 18:25:16 -0400

    number to words done

--- Theory/Week 2/Number-To-Word.cpp
+++ Theory/Week 2/Number-To-Word.cpp
@@ -1,49 +1,123 @@
 #include <iostream>
+#include <cmath>
+#include <cctype>
 using namespace std;
 
-string units[] = { "", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve", "diez", "once", "doce", "trece", "catorce", "quince", "dieciséis", "diecisiete", "dieciocho", "diecinueve" };
-string tens[] = { "", "", "veinte", "treinta", "cuarenta", "cincuenta", "sesenta", "setenta", "ochenta", "noventa" };
-string hundreds[] = { "", "cien", "doscientos", "trescientos", "cuatrocientos", "quinientos", "seiscientos", "setecientos", "ochocientos", "novecientos" };
+string units[] = { "", "uno ", "dos ", "tres ", "cuatro ", "cinco ", "seis ", "siete ", "ocho ", "nueve ", "diez ", "once ", "doce ", "trece ", "catorce ", "quince ", "dieciséis ", "diecisiete ", "dieciocho ", "diecinueve " };
+string tens[] = { "", "", "veinte ", "treinta ", "cuarenta ", "cincuenta ", "sesenta ", "setenta ", "ochenta ", "noventa " };
+string hundreds[] = { "", "ciento ", "doscientos ", "trescientos ", "cuatrocientos ", "quinientos ", "seiscientos ", "setecientos ", "ochocientos ", "novecientos " };
 
-int checkPersistency(int number){
-    int persistency = 0;
-
-    do{
-        number/= 1000;
-        persistency++;
-    }while(number > 999);
-
-    return persistency;
-}
-
-int getFirstDigits( int number, int persistency){
-    int firstDigits;
-
-    for(int i = 0; i < checkPersistency(number); i++){
-        firstDigits = number/1000;
-    }
-    return firstDigits;
-}
-
-void convertToWords(int number, int persistency){
-    if(persistency == 2){
-        
-    }
-
-}
-
-int main(){
-    int number;
-    cout << "Indique el numero a convertir en palabras(limite 999,999,999.99): ";
-    cin >> number;
+string showCents(int number){
+    string words = "";
 
     if(number == 0){
-        cout << "\ncero";
+        return words;
+    }
+    words += "con " + to_string(number) + " centavos";
+    return words;
+}
+
+string convertHundreds(int number){
+    if(number == 0){ return ""; }
+    if(number == 100){ return "cien ";};
+
+    string words = " ";
+    words += hundreds[number/100];
+    number %= 100;
+
+    if(number > 0){
+        if(number < 20){
+            words += units[number];
+            return words;
+        }
+        words += tens[number/10];
+        if(number%10 != 0){
+            words += " y " + units[number%10];
+            return words;
+        }
+    }
+}
+
+string convertThousands(int& number){
+    if(number == 0){ return ""; }
+
+    string words = " ";
+    if(number >= 1000){
+        int thousands = number/1000;
+        if(thousands == 1){
+            words += "mil ";
+            return words;
+        }
+        words += convertHundreds(thousands) + "mil";
+        number %= 1000;
     }
 
-    int persistency = checkPersistency(number);
-    int firstDigits = getFirstDigits(number, persistency);
+    return words;
+
+}
+
+string convertMillions(int& number){
+    string words = " ";
+    if(number >= 1000000){
+        int millions = number/1000000;
+        if(millions == 1){
+            words += "un millon ";
+            return words;
+        }
+        words += convertHundreds(millions) + "millones ";
+        number %= 1000000;
+    }
+
+    return words;
+}
+
+
+string convertToWords(int number, int cents){
+    if(number == 0){ return "\ncero"; }
+
+    string words = "";
+
+    words += convertMillions(number);
+    words += convertThousands(number);
+    words += convertHundreds(number);
+    words += showCents(cents);
+
+    return words;
+}
+
+
+int main(){
+    double number;
+    int cents, intPart;
+    bool validEntry = false;
+    do{
+
+    cout << "Formato de ejemplo: 784000000.78 " << endl;
+    cout << "Indique el monto a convertir en palabras(limite 999,999,999.99): ";
+    cin >> number;
     
+    if(cin.fail()){
+        cout << "Entrada inválida. Favor intentar de nuevo.\n";   
+        cout << "------------------------------------------------\n\n";   
 
+        cin.clear();
+        cin.ignore(256,'\n');
+        continue;
+    }
+    
+    if (number < 0 || number > 999999999) {
+        cout << "Número fuera de rango. Favor intentar de nuevo.\n";
+        cout << "------------------------------------------------\n\n";   
+        continue;
+    }
+        validEntry = true;
 
+    }while(!validEntry);
+
+    intPart = number;
+    cents = (number-intPart)*100;
+    
+    cout << convertToWords(number,cents) << endl;
+
+    return 0;
 }
