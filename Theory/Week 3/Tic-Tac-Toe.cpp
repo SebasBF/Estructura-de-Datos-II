d0bb51c8887522aa8608ae4a47ce1fe25e0b4e15
Sebastián Batista Ferrera <sebastianbatistaferrera@gmail.com>
2025-02-25 18:58:30 -0400

    tictactoe done

--- Theory/Week 3/Tic-Tac-Toe.cpp
+++ Theory/Week 3/Tic-Tac-Toe.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+const int winningCombinations[8][3] = {
+    {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Filas
+    {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Columnas
+    {0, 4, 8}, {2, 4, 6}             // Diagonales
+};
+
 void initializeArray(string array[]){
     for(int i = 0; i < 9; i++){
         array[i] = "~";
@@ -9,32 +15,88 @@ void initializeArray(string array[]){
 
 void showArray(string array[]){
     for(int i = 0; i < 9; i++){
-        cout << array[i] << " ";
-        if(i == 2 || i == 5){
-            cout << endl;
-        }
+        cout << array[i] << (i == 2 || i == 5 ?  "\n" : " " );
     }
 }
 
-void startGame(){
-    string cells[9];
-    initializeArray(cells);
-    showArray(cells);
+bool playPosition(int position, string array[], int player){
+    if(array[position] == "~" && player == 1){
+        array[position] = "X";
+        return true;
+    }
+    else if(array[position] == "~" && player == 2){
+        array[position] = "O";
+        return true;
+    }
+    else{
+        cout << "\nPosición inválida. Favor intentar de nuevo.\n\n";
+        return false;
+    }
 
+    return false;
 }
 
+bool endGame(string array[], string player1, string player2){
+    for (auto &combination : winningCombinations) {
+        if (array[combination[0]] != "~" && array[combination[0]] == array[combination[1]] && array[combination[1]] == array[combination[2]]) {
+            cout << "\n\nEl ganador es " << (array[combination[0]] == "X" ? player1 : player2) << endl;
+            return true;
+        }
+    }
 
-int main(){
+    // Verificar si el juego sigue en curso
+    for (int i = 0; i < 9; i++) {
+        if (array[i] == "~") return false;
+    }
 
-    string player1, player2;
-    startGame();
+    cout << "\n\nEl juego termina en empate" << endl;
+    return true;
+}
 
+void gameLoop(string player1, string player2, string cells[]){
+    int position;
+    bool gameOver = false;
+
+    while(!gameOver){
+
+        do{
+            cout << "\n\nTurno de " << player1 <<" - Inserte casilla a jugar (1 al 9): ";
+            cin >> position;  
+            cout << endl;            
+        }while(!playPosition(position - 1,cells, 1));
+        
+        showArray(cells);
+        if(endGame(cells, player1, player2)) break;
+
+        do{
+            cout << "\n\nTurno de " << player2 <<" - Inserte casilla a jugar (1 al 9): ";
+            cin >> position;    
+            cout << endl;                      
+        }while(!playPosition(position - 1,cells, 2));
+
+        showArray(cells);
+        if(endGame(cells, player1, player2)) break;
+
+    }
+       
+}
+
+void startGame(){
+    string cells[9], player1, player2;
+    initializeArray(cells);
+    
     cout << "Inserta nombre del jugador 1: ";
     cin >> player1;
 
-    cout << "\nInserta nombre del jugador 2:";
+    cout << "\nInserta nombre del jugador 2: ";
     cin >> player2;
 
+    gameLoop(player1, player2, cells);
+}
 
+int main(){
 
+    startGame();
+
+    return 0;
 }
