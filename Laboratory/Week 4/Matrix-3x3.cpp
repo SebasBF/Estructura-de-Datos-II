f490bf85d10f19202fcc75b053b76618d74d8b2f
Sebastián Batista Ferrera <sebastianbatistaferrera@gmail.com>
2025-02-28 17:36:38 -0400

    .

--- Laboratory/Week 4/Matrix-3x3.cpp
+++ Laboratory/Week 4/Matrix-3x3.cpp
@@ -1,52 +1,75 @@
 #include <iostream>
-#include <cstdlib>  // Para rand() y srand()
-#include <ctime>    // Para time()
-#include <algorithm> // Para shuffle()
-
 using namespace std;
 
-const int winningCombinations[8][3] = {
-    {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Filas
-    {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Columnas
-    {0, 4, 8}, {2, 4, 6}             // Diagonales
-};
+int contador = 0; 
 
-bool isSafe(const int tablero[3][3], int row, int col, int num) {
-    for (int i = 0; i < 3; i++) {
-        if (tablero[row][i] == num + tablero[i][col] == num) {
+bool isSafe(int tablero[3][3], int num) {
+    for(int y = 0 ; y < 3; y++){
+    for (int x = 0; x < 3; x++) {
+        if (tablero[y][x] == num){
             return false;
         }
-    }
+    }}
     return true;
 }
-// Función para inicializar la matriz con números únicos del 1 al 9
-void checkRow(int matrix[3][3], int first, int second, int third) {
-    for(int i = 1; i < 3; i++){
-        if(first + second + third != 15){
-            return checkRow()
-        }
-    }
-}
 
-// Función para imprimir la matriz
-void imprimirMatriz(int matrix[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
 
-bool isValid(const int tablero[3][3], char caracter) {
+bool cumpleCondicion(int matrix[3][3]) {
+    for (int i = 0; i < 3 ; i++) {
+        if (matrix[i][0] + matrix[i][1] + matrix[i][2] != 15 ) return false;
+        if (matrix[0][i] + matrix[1][i] + matrix[2][i] != 15) return false;
+    }
+    if (matrix[0][0] + matrix[1][1] + matrix[2][2] != 15 ) return false;
+    if (matrix[0][2] + matrix[1][1] + matrix[2][0] != 15 ) return false;
    
+    return true;
+} 
+
+void solve(int matrix[3][3], int row, int col) {
+    if (row == 3)  //Una vez rellenamos todas las filas, revisamos si se cumple.
+    {
+        if (cumpleCondicion(matrix))
+        {
+            cout << "\nSolución encontrada después de " << contador << " iteraciones:\n";
+            cout << endl;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    cout << " " << matrix[i][j];
+                    if (j < 2)
+                        cout << " |";
+                }
+                cout << endl;
+                if (i < 2)
+                    cout << "---+---+---\n";
+            }
+            cout << endl;
+        }
+        return;
+    }
+
+    if (col == 3) //Luego de que rellenamos una fila, continuamos con la otra
+    {
+        solve(matrix, row + 1, 0);
+        return;
+    }
+
+    for (int i = 1; i <= 9; i++)  //Caso base, inicia aqui
+    {
+        contador++;  // Incrementamos el contador en cada intento
+        if (isSafe(matrix, i))
+        {
+            matrix[row][col] = i;
+            solve(matrix, row, col + 1);
+
+            matrix[row][col] = 0;
+        }
+    }
 }
-int main() {
-    int matrix[3][3];
+int main(){
 
-    // Inicializar y mostrar la matriz
-    inicializarMatriz(matrix);
-    imprimirMatriz(matrix);
-
+    int matrix[3][3] = {0};
+    solve(matrix,0,0);
     return 0;
 }
