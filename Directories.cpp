ad3fd6e066f753ec270e64a04f8ab7f54125af5d
SebasBF <sebastianbatistaferrera@gmail.com>
2025-03-12 20:36:30 -0400

    directories done

--- Directories.cpp
+++ Directories.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
 #include <dirent.h>
+#include <sys/stat.h>
 
 using namespace std;
 
-void list_dir(string dir){
+bool esDirectorio(const string& path) {
+    struct stat info;
+    if (stat(path.c_str(), &info) != 0) {
+        cerr << "Error al acceder a la ruta: " << path << endl;
+        return false;
+    }
+    return S_ISDIR(info.st_mode);
+}
+
+void list_dir(string dir, string extention){
     DIR * directorio;
-    struct dirent * elemento;
+    struct dirent* elemento;
     string elem;
 
     if(directorio = opendir(dir.c_str())){
 
         while (elemento = readdir(directorio)){
-            elem = elemento->d_name;
-            cout << elem << endl;
+            
+            elem = elemento -> d_name;
+            size_t posicionPunto = elem.find_last_of(".");
+            string extension = elem.substr(posicionPunto + 1);
+            string fullPath = dir + "\\" + elem; 
+
+            if (elem == "." || elem == "..") {
+                continue;
+            }
+
+            if (esDirectorio(fullPath)) {
+                
+                list_dir(fullPath, extension); 
+
+            } else {
+                size_t posicionPunto = elem.find_last_of(".");
+                
+                    string fileExtension = elem.substr(posicionPunto + 1);
+                    if (fileExtension == extension) {
+                        cout << "Archivo encontrado: " << fullPath << endl;
+                    }
+                
+            }
+            
         }
         
     }
@@ -21,12 +53,15 @@ void list_dir(string dir){
 }
 
 void init(){
-    cout << "Ruta del directorio a listar: ";
-    string dir;
-    getline(cin,dir);
-    list_dir(dir);
+    string extension;
+    string dir = "C:\\Users\\1122881\\Desktop\\Directories";
+    cout << "Ruta del directorio a listar: " << dir;
+    cout << "\nIndique la extension a listar: ";
+    cin >> extension;
 
-    init();
+    list_dir(dir, extension);
+
+   
 }
 
 int main(){
